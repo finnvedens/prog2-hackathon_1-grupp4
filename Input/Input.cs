3f2c738184e0b36cd7c4afb9c9f626f00e071548
andrewgalb <andrew.f.galbraith@edu.varnamo.se>
2016-02-26 08:17:01 +0100

    Hackathon 1

---
+++ Input/Input.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Input
+{
+    class Input
+    {
+        List<string> Keywords = new List<string>();
+
+        public Input()
+        {
+            Keywords.Add("attack");
+            Keywords.Add("search");
+            Keywords.Add("left");
+            Keywords.Add("right");
+            Keywords.Add("forward");
+        }
+
+        public int getKeyword(string input)
+        {
+            List<string> Words = new List<string>();
+
+            string a = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    Words.Add(a);
+                    a = "";
+                }
+                else
+                {
+                    a += input[i];
+                }
+            }
+            Words.Add(a);
+
+            for (int i = 0; i < Keywords.Count; i++)
+            {
+                if (Words[0] == Keywords[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Input/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input i = new Input();
+
+            Console.WriteLine(i.getKeyword("attack"));
+        }
+    }
+}
